b5cab5ca47a2960913ad4d4435850879b3acff75
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-22 19:04:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
 	end
 
 
-def get_japanese_emoticon	def get_english_meaning(emoticon_file, emoticon)
+def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   english_meaning = emoticon_lib['get_meaning'][emoticon]
   english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
