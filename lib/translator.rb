03d07fc4b45a37883e107e831b066d97ab8dc4f9
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-22 18:58:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,6 @@ require 'pry'
 def load_library(path)
   emoticons=YAML.load_file(path)
   emoticon_hash=Hash.new
-  emoticon_hash["get_emoticon"]=Hash.new
-  emoticon_hash["get_meaning"]=Hash.new
+  emoticon_hash[:get_emoticon]=Hash.new
+  emoticon_hash[:get_meaning]=Hash.new
 end
